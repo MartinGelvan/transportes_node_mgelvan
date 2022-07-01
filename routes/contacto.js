f9dcc13db195993ca88eb6e449b3051c4a21b300
MartinGelvan <martingelvan@gmail.com>
2022-07-01 11:55:55 -0300

    funcionamiento del envio del mail

--- routes/contacto.js
+++ routes/contacto.js
@@ -1,10 +1,50 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
 
-router.get('/',function(req,res,next){
-    res.render('contacto',{
-        isContacto:true
+router.get('/', function (req, res, next) {
+    res.render('contacto', {
+        isContacto: true
     }); // view/contacto.hbs
 })
+
+router.post('/', async function (req, res, next) {
+    //console.log(req.body)
+
+
+    //capturando los datos
+    var nombre = req.body.nombre;
+    var email = req.body.email;
+    var tel = req.body.tel;
+    var comentarios = req.body.comentarios;
+    //onsole.log(req.body.nombre);
+
+    var obj = {
+        to: 'madstnadagadaeadlvn@gmail.com',
+        subject: 'Contacto desde la pagina web',
+        html: nombre + " se contacto a traves de la web y quiere saber mas info a este correo: " + email + "<br> Su tel es: " + tel + " . Su comentario es: " + comentarios + "."
+
+    }
+
+    var transport = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS
+        }
+    }) // finaliza el transport
+
+    var info = await transport.sendMail(obj);
+
+    res.render('contacto', {
+        message: 'Mensaje enviado correctamente',
+        isContacto: true
+    })
+
+}); // finaliza router.post
+
+
+
 
 module.exports = router;

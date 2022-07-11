7c7b599a23d48e04deb0580526f34499271f6c7c
MartinGelvan <martingelvan@gmail.com>
2022-07-11 11:09:23 -0300

    Tabla novedades - listar

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require('../../models/novedadesModel');
 
-
-router.get('/',function(req,res,next){
+router.get('/',async function(req,res,next){
+    var novedades = await novedadesModel.getNovedades();
     res.render('admin/novedades',{
         layout:'admin/layout', //admin/layout.hbs
-        persona:req.session.nombre //flavia 
+        persona:req.session.nombre,//flavia 
+        novedades
     }); //view/admin/novedades.hbs
 })
 

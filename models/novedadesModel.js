0487b2912c8b1d9c4052d946a4a05f73c1557e81
MartinGelvan <martingelvan@gmail.com>
2022-07-15 11:20:24 -0300

    Modificar novedades

--- models/novedadesModel.js
+++ models/novedadesModel.js
@@ -26,4 +26,23 @@ async function deleteNovedadById(id){
 
 }
 
-module.exports = { getNovedades, insertNovedades , deleteNovedadById }
+//Traer la novedad dentro del form
+async function getNovedadesById(id){
+    var query = 'select * from novedades where id= ?';
+    var rows = await pool.query(query,[id]);
+    return rows[0];
+
+}
+
+async function modificarNovedadById(obj,id){
+    try{
+        var query = 'update novedades set ? where id= ?';
+        var rows = await pool.query(query,[obj,id]);
+        return rows;
+
+    }catch(error){
+        console.log(error);
+    }
+}
+
+module.exports = { getNovedades, insertNovedades , deleteNovedadById , getNovedadesById , modificarNovedadById }
--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -4,55 +4,89 @@ var novedadesModel = require('../../models/novedadesModel');
 
 // diseño y listado de novedades
 
-router.get('/',async function(req,res,next){
+router.get('/', async function (req, res, next) {
     var novedades = await novedadesModel.getNovedades();
-    res.render('admin/novedades',{
-        layout:'admin/layout', //admin/layout.hbs
-        persona:req.session.nombre,//flavia 
+    res.render('admin/novedades', {
+        layout: 'admin/layout', //admin/layout.hbs
+        persona: req.session.nombre,//flavia 
         novedades
     }); //view/admin/novedades.hbs
 })
 
 // esto sirve para mostrar form el alta las novedades
 
-router.get('/agregar',(req,res,next)=>{
-    res.render('admin/agregar',{ //agregar.hbs
+router.get('/agregar', (req, res, next) => {
+    res.render('admin/agregar', { //agregar.hbs
         layout: 'admin/layout'
     })
 })
 
 //agregar novedad
 
-router.post('/agregar',async (req,res,next)=>{
+router.post('/agregar', async (req, res, next) => {
     //console.log(req.body);
-    try{
-        if(req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != ""){
+    try {
+        if (req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != "") {
             await novedadesModel.insertNovedades(req.body);
             res.redirect('/admin/novedades');
-        }else{
-            res.render('admin/agregar',{
-                layout:'admin/layout',
-                error:true,
-                message:'Todos los campos son requeridos'
+        } else {
+            res.render('admin/agregar', {
+                layout: 'admin/layout',
+                error: true,
+                message: 'Todos los campos son requeridos'
             })
         }
-    }catch(error){
+    } catch (error) {
         console.log(error);
-        res.render('admin/agregar',{
-            layout:'admin/layout',
-            error:true,
-            message:'No se carga la novedad'
+        res.render('admin/agregar', {
+            layout: 'admin/layout',
+            error: true,
+            message: 'No se carga la novedad'
         })
     }
 })
 
 /*eliminar novedad*/
 
-router.get('/eliminar/:id', async (req,res,next)=>{
+router.get('/eliminar/:id', async (req, res, next) => {
     //console.log(req.params.id);
     var id = req.params.id;
     await novedadesModel.deleteNovedadById(id);
     res.redirect('/admin/novedades');
 })
 
+/*Vista modificar (form) + los datos de campos para modificar*/
+
+router.get('/modificar/:id', async (req, res, next) => {
+    var id = req.params.id;
+    var novedad = await novedadesModel.getNovedadesById(id);
+    res.render('admin/modificar', {
+        layout: 'admin/layout',
+        novedad
+    })
+})
+
+
+//Actualizacion de los datos
+
+router.post('/modificar', async (req, res, next) => {
+    try {
+        var obj = {
+            titulo: req.body.titulo,
+            subtitulo: req.body.subtitulo,
+            cuerpo: req.body.cuerpo
+        }
+
+        await novedadesModel.modificarNovedadById(obj, req.body.id);
+        res.redirect('/admin/novedades');
+
+    } catch (error) {
+        console.log(error);
+        res.render('admin/modificar', {
+            layout: 'admin/layout',
+            error: true,
+            message: 'No se modifico la novedad'
+        })
+    }
+})
 module.exports = router;
